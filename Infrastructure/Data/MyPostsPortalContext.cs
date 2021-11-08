5e817dbe1ccc85bd0f8befc1f2fd35c3baf4bf53
FFad3 <gnebapatryk@gmail.com>
2021-11-08 23:55:20 +0100

    Create DbContext

--- Infrastructure/Data/MyPostsPortalContext.cs
+++ Infrastructure/Data/MyPostsPortalContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
-    internal class MyPostsPortalContext
+    public class MyPostsPortalContext : DbContext
     {
+        public MyPostsPortalContext(DbContextOptions options) : base(options) { }
+
+        //Accounts          
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountDetails> AccountDetails { get; set; }
+
+        //Posts
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostContent> PostContents{ get; set; }
+
+        //Others
+        public DbSet<Comment> Comments{ get; set; }
+        public DbSet<Opinion> Opinions { get; set; }
+
     }
 }
