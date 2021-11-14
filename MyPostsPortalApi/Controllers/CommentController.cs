fe6c9d3b1061d54c90c189ffd4f3c47f879f16c1
FFad3 <gnebapatryk@gmail.com>
2021-11-14 16:33:40 +0100

    CommentController

---
+++ MyPostsPortalApi/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Dtos.DtoOther;
+using Application.Dtos.DtoPost;
+using Application.ServiceInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace MyPostsPortalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService _service;
+        public CommentController(ICommentService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("~/Comment/Create"),SwaggerOperation("Add new comment")]
+        public IActionResult CreatePost(CreateCommentDto newPost)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _service.CreateComment(newPost);
+                return Ok(result);
+            }
+            return BadRequest();    
+        }
+    }
+}
